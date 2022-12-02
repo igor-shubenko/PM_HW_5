83b9f1a13679ccb6688bf90d493ccc47f4229600
Igor Shubenko <ispectremrpl@gmail.com>
2022-12-02 22:43:55 +0200

    Refactored math_server

--- math_server_app/math_operations_server.py
+++ math_server_app/math_operations_server.py
@@ -1,50 +1,29 @@
-import os
-
-from fastapi import FastAPI
-import asyncio
+from fastapi import FastAPI, Request
 import uvicorn
-import httpx
-import concurrent.futures
-from math_operations import MedianCalculator, UniqueNamesCalculator, AgeRangeCalculator
 
-math_operations_server = FastAPI(title="Math Operations Server",
-                                 description="Server provides math operations with users data",
-                                 version="2.0")
+from handlers.event_handlers import startup_event_handler, shutdown_event_handler
 
-url = os.getenv("CRUD_SERVER_LINK")
-client = httpx.AsyncClient()
-executor = concurrent.futures.ThreadPoolExecutor()
+app = FastAPI(title="Math Operations Server",
+              description="Server provides math operations with users data",
+              version="2.0")
 
-@math_operations_server.get("/test")
-async def test():
-    response = await client.get(url, timeout=100)
-    return response.json()
+app.add_event_handler('startup', startup_event_handler(app))
+app.add_event_handler('shutdown', shutdown_event_handler(app))
+
+@app.get("/median")
+async def median(request: Request):
+    return await request.app.task_maker.median()
 
 
-@math_operations_server.get("/median")
-async def median():
-    response = await client.get(url, timeout=100)
-    loop = asyncio.get_running_loop()
-    inst = MedianCalculator(response.json())
-    result = await loop.run_in_executor(executor, inst)
-    return result
+@app.get("/unique_names_histogram")
+async def unique_names_histogram(request: Request):
+    return await request.app.task_maker.unique_names_histogram()
 
-@math_operations_server.get("/unique_names_histogram")
-async def unique_names_histogram():
-    response = await client.get(url, timeout=100)
-    loop = asyncio.get_running_loop()
-    inst = UniqueNamesCalculator(response.json())
-    result = await loop.run_in_executor(executor, inst)
-    return result
 
-@math_operations_server.get("/age_range")
-async def age_range(frm: int, to: int):
-    response = await client.get(url, timeout=100)
-    loop = asyncio.get_running_loop()
-    inst = AgeRangeCalculator(response.json())
-    result = await loop.run_in_executor(executor, inst, frm, to)
-    return result
+@app.get("/age_range")
+async def age_range(request: Request, frm: int, to: int):
+    return await request.app.task_maker.age_range(frm, to)
 
 
 if __name__ == '__main__':
-    uvicorn.run("math_operations_server:math_operations_server", host='0.0.0.0', port=8766, reload=True)
+    uvicorn.run("math_operations_server:app", host='0.0.0.0', port=8766, reload=True)
